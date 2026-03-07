119b5d3c600cdba8821d64b924a40cc83d1f0518
Gopher <1900271083@qq.com>
2026-03-08 00:50:41 +0800

    add config.fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+# 别名
+alias ls="lsd -l"
+
+# Go 环境
+set -Ux PATH /usr/local/go/bin $PATH
+set -gx PATH $HOME/go/bin $PATH
