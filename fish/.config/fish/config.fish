bb2f811882598bcb4f76fc4c8e505b8403c6ec93
Gopher <1900271083@qq.com>
2026-03-11 00:21:22 +0800

    add Rust Basic Env

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,11 +3,15 @@ if status is-interactive
 end
 
 # 别名
+alias l="command ls"
 alias ls="lsd -l"
 
 # Go 环境
 set -Ux PATH /usr/local/go/bin $PATH
 set -gx PATH $HOME/go/bin $PATH
+
+# Cargo
+set -gx PATH $HOME/.cargo/bin $PATH
 
 # zoxide
 zoxide init fish | source
