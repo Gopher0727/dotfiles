6a2f1a9c3b374bdef4bff243b9f6fe5a18e6d733
Gopher <gopher0727@gmail.com>
2026-06-29 21:32:52 +0800

    upd

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -24,3 +24,34 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
 vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "Registers" })
 vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
+vim.keymap.set("n", "<leader>ft", function()
+	local theme_file = vim.fn.stdpath("data") .. "/selected_theme"
+	local themes = vim.fn.getcompletion("", "color")
+	require("telescope.pickers").new({}, {
+		prompt_title = "Themes",
+		finder = require("telescope.finders").new_table({ results = themes }),
+		sorter = require("telescope.config").values.generic_sorter({}),
+		previewer = require("telescope.previewers").new_buffer_previewer({
+			define_preview = function(_, entry)
+				vim.cmd.colorscheme(entry.value)
+			end,
+		}),
+		attach_mappings = function(prompt_bufnr)
+			local actions = require("telescope.actions")
+			local state = require("telescope.actions.state")
+			actions.select_default:replace(function()
+				local selection = state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				if selection then
+					vim.cmd.colorscheme(selection.value)
+					local f = io.open(theme_file, "w")
+					if f then
+						f:write(selection.value)
+						f:close()
+					end
+				end
+			end)
+			return true
+		end,
+	}):find()
+end, { desc = "Find theme" })
--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,6 +1,27 @@
 vim.pack.add({
 	{ src = "https://github.com/catppuccin/nvim" },
 	{ src = "https://github.com/folke/tokyonight.nvim" },
+	{ src = "https://github.com/tanvirtin/monokai.nvim" },
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
 })
 
-vim.cmd.colorscheme("tokyonight")
+require("tokyonight").setup({
+	style = "night",
+})
+require("catppuccin").setup({
+	flavour = "mocha",
+})
+
+-- Persist selected theme
+local theme_file = vim.fn.stdpath("data") .. "/selected_theme"
+local default = "default"
+local theme = default
+local f = io.open(theme_file, "r")
+if f then
+	local saved = f:read("*l"):gsub("%s+", "")
+	f:close()
+	if saved ~= "" and vim.tbl_contains(vim.fn.getcompletion("", "color"), saved) then
+		theme = saved
+	end
+end
+vim.cmd.colorscheme(theme)
