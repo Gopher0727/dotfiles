715c14099b11458fd31998e24b11d5c4d913ce10
Gopher <gopher0727@gmail.com>
2026-09-25 18:59:06 +0800

    upd

---
+++ mini-nvim/init.lua
@@ -0,0 +1,11 @@
+vim.o.nu = true
+vim.o.rnu = true
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.clipboard = "unnamedplus"
+vim.o.inccommand = "split"
+vim.o.autoread = true
+
+vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>")
+vim.keymap.set("i", "<Esc>", "<Esc><cmd>noh<CR>")
